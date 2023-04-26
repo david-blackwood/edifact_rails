6941619089efe8c2e02d72b2db57abf6c9e081b3
David Lowndes <david.lowndes@firstb2b.com>
2023-04-26 14:10:53 +0100

    get file input sorted

--- lib/edifact_rails/parser.rb
+++ lib/edifact_rails/parser.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'byebug'
 
 module EdifactRails
   class Parser
@@ -8,37 +9,68 @@ module EdifactRails
     ESCAPE_CHARACTER = "?"
 
     def parse(string)
-      result = []
-
-      # This fancy regex means
-      # "Don't match escaped characters (but do if the escape character itself is escaped)"
-      segments = string.split(
-        /(?<!(?<!#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::SEGMENT_SEPARATOR)}/
-      )
-
-      segments.each do |segment|
-        result.push parse_segment(segment)
-      end
-
-      result
+      parse_input(string)
     end
 
-    def parse_file
-      File.foreach(file_path) do |line|
-        puts line
-      end
+    def parse_file(file_path)
+      parse_input(File.read(file_path).split("\n").join)
     end
 
     private
 
+    # The input is one large string, split into segments
+    def parse_input(input)
+      parsed_segments = []
+
+      # If there is an EVEN number of escape characters before another special character, the special character is therefore unescaped.
+      # Add a space between these even number of escapes, and the special character
+      # This means the regex logic for #splitting on special characters is now consistent, since we now know that if there
+      # is an escape character before the special character, it should definately be respected
+      # We have to do this because we can't negative lookbehind for 'an even number of escape characters' since 
+      # lookbehinds have to be fixed length.
+      # The added space, which is now at the boundry of a component,
+      # will get cut by the #strip! in parse_component eventually
+
+      # "LIN+even????+123" => '+' is not escaped, gsub'ed => "even???? +123" => parsed => ['LIN', ['even??'], [123]]
+      # "LIN+odd???+123" => '+' is escaped, not gsub'ed => "odd???+123" => parsed => ['LIN', ['odd?+123']]
+
+      # Making the regex more readable
+      escape = Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)
+      other_specials = Regexp.quote(
+        [
+          EdifactRails::Parser::SEGMENT_SEPARATOR,
+          EdifactRails::Parser::DATA_ELEMENT_SEPARATOR,
+          EdifactRails::Parser::COMPONENT_DATA_ELEMENT_SEPARATOR
+        ].join
+      )
+
+      input = input.gsub(/(?<!#{escape})((#{escape}{2})+)([#{other_specials}])/, '\1 \3')
+
+      segments = input.split(
+        /(?<!#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::SEGMENT_SEPARATOR)}/
+      )
+
+
+      segments.reject! { |s| s[0..2] == 'UNA' }
+
+      segments.each do |segment|
+        parsed_segments.push parse_segment(segment)
+      end
+
+      parsed_segments
+    end
+
+    # Segments are split into data element, where the first data element is the 'tag'
     def parse_segment(segment)
       # Segments are made up of data elements
       data_elements = segment.split(
-        /(?<!(?<!#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::DATA_ELEMENT_SEPARATOR)}/
+        /(?<!#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::DATA_ELEMENT_SEPARATOR)}/
       )
 
+      parsed_segment = []
+      
       # The first element is the tag, pop it off
-      parsed_segment = [data_elements.shift]
+      parsed_segment.push(data_elements.shift) if data_elements.any?
 
       data_elements.each do |cell|
         parsed_segment.push parse_data_element(cell)
@@ -47,11 +79,12 @@ module EdifactRails
       parsed_segment
     end
 
+    # Data elements are split into component data elements
     def parse_data_element(element)
       parsed_element = []
 
       components = element.split(
-        /(?<!(?<!#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::COMPONENT_DATA_ELEMENT_SEPARATOR)}/
+        /(?<!#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)})#{Regexp.quote(EdifactRails::Parser::COMPONENT_DATA_ELEMENT_SEPARATOR)}/
       )
 
       all_special_characters = [
@@ -62,15 +95,18 @@ module EdifactRails
       ].join
 
       components.each do |component|
-        component = component.chomp
+        # Remove surrounding whitespace
+        component.strip!
 
         # If the component has escaped characters in it, remove the escape character and return the character as is
-        # "?+" -> "+"
-        # "??" -> "?"
+        # "?+" -> "+", "??" -> "?"
         component.gsub!(
           /#{Regexp.quote(EdifactRails::Parser::ESCAPE_CHARACTER)}([#{Regexp.quote(all_special_characters)}])/,
           '\1'
         )
+
+        # Convert empty strings to nils
+        component = nil if component.empty?
 
         # Convert the component to integer if it is one
         # "1" -> 1
--- spec/edifact_rails_spec.rb
+++ spec/edifact_rails_spec.rb
@@ -2,39 +2,86 @@
 
 require "spec_helper"
 require "edifact_rails"
+require 'byebug'
+
+FILES_DIR = File.dirname(__FILE__) + "/test_files"
 
 RSpec.describe EdifactRails do
-  context "with a string input" do
-    it "parses a single line" do
-      result = described_class.parse("UNB+IATB:1+6XPPC+LHPPC+940101:0950+1'")
-      expect(result).to eq([
-                             ["UNB", ["IATB", 1], ["6XPPC"], ["LHPPC"], [940101, "0950"], [1]]
-                           ])
-    end
-
-    it "parses multiple lines" do
-      result = described_class.parse("LIN+1+1+0764569104:IB'QTY+1:25'")
-
-      expect(result).to eq([
-                             ["LIN", [1], [1], ["0764569104", "IB"]],
-                             ["QTY", [1, 25]]
-                           ])
-    end
-
-    it "handles escaped characters" do
-      result = described_class.parse("LIN+?+?:?'??:1+A Giant?'s tale?::Does One ?+ Two = Trouble??+156")
-
-      expect(result).to eq([["LIN", ["+:'?", 1], ["A Giant's tale:", "Does One + Two = Trouble?"], [156]]])
-    end
-
-    it "handles empty segments"
-
-    it "handles empty data elements"
-
-    it "handles empty data components"
+  it "parses a single line" do
+    result = described_class.parse("UNB+IATB:1+6XPPC+LHPPC+940101:0950+1'")
+    expect(result).to eq([
+                            ["UNB", ["IATB", 1], ["6XPPC"], ["LHPPC"], [940101, "0950"], [1]]
+                          ])
   end
 
-  context "with a file input" do
-    it "returns anything"
+  it "parses multiple lines" do
+    result = described_class.parse("LIN+1+1+0764569104:IB'QTY+1:25'")
+
+    expect(result).to eq([
+                            ["LIN", [1], [1], ["0764569104", "IB"]],
+                            ["QTY", [1, 25]]
+                          ])
+  end
+
+  it "handles escaped characters" do
+    result = described_class.parse("LIN+?+?:?'??:1+A Giant?'s tale?::Does One ?+ Two = Trouble????+156")
+
+    expect(result).to eq([["LIN", ["+:'?", 1], ["A Giant's tale:", "Does One + Two = Trouble??"], [156]]])
+  end
+
+  it "handles empty segments" do
+    result = described_class.parse("QTY+1''QTY+2")
+
+    expect(result).to eq([
+      ['QTY', [1]],
+      [],
+      ['QTY', [2]]
+    ])
+  end
+
+  it "handles empty data elements" do
+    result = described_class.parse("FTX+AFM+1++Java Server Programming'")
+
+    expect(result).to eq([
+      ['FTX', ['AFM'], [1], [], ['Java Server Programming']]
+    ])
+  end
+
+  it "handles empty data components" do
+    result = described_class.parse("PDI++C:3+Y::3+F::1+A'")
+
+    expect(result).to eq([
+      ['PDI', [], ['C', 3], ['Y', nil, 3], ['F', nil, 1], ['A']]
+    ])
+  end
+
+  it "parses a file" do
+    result = described_class.parse_file("#{FILES_DIR}/seperate_lines.edi")
+    expected = [
+      ['UNB', ["UNOA", 3], ["TESTPLACE", 1], ["DEP1", 1], [20051107, 1159], [6002]],
+      ['UNH', ["SSDD1"], ["ORDERS", "D", "03B", "UN", "EAN008"]],
+      ['BGM', [220], ["BKOD99"], [9]],
+      ['DTM', [137, 20051107, 102]],
+      ['NAD', ["BY"], [5412345000176, nil, 9]],
+      ['NAD', ["SU"], [4012345000094, nil, 9]],
+      ['LIN', [1], [1], ["0764569104", "IB"]],
+      ['QTY', [1, 25]],
+      ['FTX', ["AFM"], [1], [], ["XPath 2.0 Programmer's Reference"]],
+      ['LIN', [2], [1], ["0764569090", "IB"]],
+      ['QTY', [1, 25]],
+      ['FTX', ["AFM"], [1], [], ["XSLT 2.0 Programmer's Reference"]],
+      ['LIN', [3], [1], [1861004656, "IB"]],
+      ['QTY', [1, 16]],
+      ['FTX', ["AFM"], [1], [], ["Java Server Programming"]],
+      ['LIN', [4], [1], ["0596006756", "IB"]],
+      ['QTY', [1, 10]],
+      ['FTX', ["AFM"], [1], [], ["Enterprise Service Bus"]],
+      ['UNS', ["S"]],
+      ['CNT', [2, 4]],
+      ['UNT', [22], ["SSDD1"]],
+      ['UNZ', [1], [6002]]
+    ]
+
+    expect(result).to eq(expected)
   end
 end
